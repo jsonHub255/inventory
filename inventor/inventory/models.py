e2877507f04cc44812c1fb4a8107483f951a2585
Json <jsonlli255@gmail.com>
2021-03-29 20:29:40 +0200

    Create "Engin Model, Before migrate!.

--- inventor/inventory/models.py
+++ inventor/inventory/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Engin(models.Model):
+    eng_name = models.CharField(max_length=64)
+    eng_code = models.CharField(max_length=64)
+    ch_name = models.CharField(max_length=64)
+    eng_date = models.DateField
+    
+    def __str__(self):
+        return f"{id} For: name: {self.eng_name} Code: {self.eng_code} Chantier: {self.ch_name} DATE: {self.eng_date}"
+    
