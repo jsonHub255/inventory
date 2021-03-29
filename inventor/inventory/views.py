568ce285334c54b481b2e08ce63f5b45d6eee7e9
Json <jsonlli255@gmail.com>
2021-03-29 20:20:31 +0200

    add index.html, before edit modules classes.

--- inventor/inventory/views.py
+++ inventor/inventory/views.py
@@ -1,3 +1,7 @@
+from django.http.response import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "inventory/index.html")
